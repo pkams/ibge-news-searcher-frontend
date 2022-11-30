0bdf051a758f326487646b8690602a2dd7dd65c9
Patrick Souza <patrick_ams@hotmail.com>
2022-11-30 00:35:20 -0300

    Updates

---
+++ precache-manifest.d71f5d78ec2d1e61b3af6ceeec88a2bf.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "aaf83380aff973fe3f99",
+    "url": "/ibge-news-searcher-frontend/static/css/main.7a56989c.chunk.css"
+  },
+  {
+    "revision": "aaf83380aff973fe3f99",
+    "url": "/ibge-news-searcher-frontend/static/js/main.01986131.chunk.js"
+  },
+  {
+    "revision": "5d1c9d1443218ccfb9eb",
+    "url": "/ibge-news-searcher-frontend/static/js/runtime~main.7be02ec6.js"
+  },
+  {
+    "revision": "49f2f3934a0174ca815c",
+    "url": "/ibge-news-searcher-frontend/static/js/2.299ff3d1.chunk.js"
+  },
+  {
+    "revision": "eebb968f37e6c1cd7a17",
+    "url": "/ibge-news-searcher-frontend/static/js/3.8f80cc39.chunk.js"
+  },
+  {
+    "revision": "0fe17cd93622911f19953d2593fd867b",
+    "url": "/ibge-news-searcher-frontend/static/media/news.0fe17cd9.svg"
+  },
+  {
+    "revision": "ed533866b5c83114c7dddbcbc2288b19",
+    "url": "/ibge-news-searcher-frontend/static/media/Inter-Medium.ed533866.ttf"
+  },
+  {
+    "revision": "079af0e2936ccb99b391ddc0bbb73dcb",
+    "url": "/ibge-news-searcher-frontend/static/media/Inter-Regular.079af0e2.ttf"
+  },
+  {
+    "revision": "980c7e8757e741bb49c7c96513924c61",
+    "url": "/ibge-news-searcher-frontend/static/media/Inter-Black.980c7e87.ttf"
+  },
+  {
+    "revision": "275bfea5dc74c33f51916fee80feae67",
+    "url": "/ibge-news-searcher-frontend/static/media/Inter-Bold.275bfea5.ttf"
+  },
+  {
+    "revision": "0be9ced1420fd4406f59444a6546c81b",
+    "url": "/ibge-news-searcher-frontend/index.html"
+  }
+];
